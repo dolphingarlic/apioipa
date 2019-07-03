f2d54a5ed4f8b8b18b4baaeebb285fe348324379
Andi Qu <dolphingarlic@gmail.com>
2019-07-02 17:54:48 -0700

    '

--- cpbot/settings.py
+++ cpbot/settings.py
@@ -10,6 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
+import dj_database_url
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -138,6 +140,8 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
-import dj_database_url
 prod_db = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
+
+
+django_heroku.settings(locals())
