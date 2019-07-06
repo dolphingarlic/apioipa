d21632cea20385a2469ec0b446351a76c6ad0024
Andi Qu <dolphingarlic@gmail.com>
2019-07-06 11:43:43 +0400

    Added natural key for Source

--- problems/models.py
+++ problems/models.py
@@ -1,9 +1,16 @@
 from django.db import models
 
 
+class SourceManager(models.Manager):
+    def get_by_natural_key(self, abbreviation):
+        return self.get(abbreviation=abbreviation)
+
+
 class Source(models.Model):
     abbreviation = models.CharField(max_length=100)
     name = models.CharField(max_length=100)
+
+    objects = SourceManager()
 
     def __str__(self):
         return self.name
@@ -12,7 +19,7 @@ class Source(models.Model):
 class Problem(models.Model):
     name = models.CharField(max_length=100)
     source = models.ForeignKey(Source, on_delete=models.SET_NULL, null=True)
-    url = models.URLField(default='https://oj.uz/')
+    url = models.URLField()
     from_year = models.IntegerField()
 
     def __str__(self):
